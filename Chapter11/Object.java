80015dd9985b994753bdcc706b9b470d4ae2af68
Allie O'Brien <obrienaj@s.dcsdk12.org>
2019-08-23 14:41:07 -0600

    Add Object exercise

---
+++ Chapter11/Object.java
@@ -0,0 +1,15 @@
+public class Object{
+    public static void main(String args[]){
+
+    String str = new String("Hello World");
+    System.out.println( str );
+
+    String secondVariable = new ;
+    System.out.println( secondVariable );
+
+
+    }
+
+
+
+}
