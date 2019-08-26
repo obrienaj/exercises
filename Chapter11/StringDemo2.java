be2af6e7ba80453bad96d27c92e10e70d76bcbb4
Allie O'Brien <obrienaj@s.dcsdk12.org>
2019-08-26 14:33:11 -0600

    finish Chapter 11

--- Chapter11/StringDemo2.java
+++ Chapter11/StringDemo2.java
@@ -1,21 +1,20 @@
 public class StringDemo2{
     public static void main (String[] args){
-        String str;
-        int len;
-
-        str = new String("       Cats!!!        ");
-
-        len = str.length();
+     
+        String str = new String("       Cats are nice!!!        ");
+        int len = str.length();
 
         System.out.println(str);
         System.out.println("The length is: " + len);
 
         String second = str.trim();
-
         int len2 = second.length();
 
         System.out.println(second);
         System.out.println("The length is now: " + len2);
+
+        String sub = second.substring(10, len2);
+        System.out.println(sub);
 
 
     }
