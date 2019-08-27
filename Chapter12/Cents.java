d9b733b50a3657a40a38dc2379159215a9c07b91
Allie O'Brien <obrienaj@s.dcsdk12.org>
2019-08-27 14:46:21 -0600

    Finish Cents

--- Chapter12/Cents.java
+++ Chapter12/Cents.java
@@ -3,7 +3,16 @@ import java.util.Scanner;
 public class Cents{
     public static void main(String[] args){
 
+    Scanner scan = new Scanner(System.in);
+    int cents, dollars;
 
+    System.out.println("Input cent value: ");
+    cents = scan.nextInt();
+
+    dollars = cents/100;
+    cents = cents%100;
+
+    System.out.println("That is " + dollars + " dollars and " + cents + " cents.");
 
     }
 
