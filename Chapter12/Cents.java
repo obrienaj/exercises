cbd9e62bee9160b9a6809d903bb514fd99e2399d
Allie O'Brien <obrienaj@s.dcsdk12.org>
2019-08-28 13:32:38 -0600

    Finish Cents for real

--- Chapter12/Cents.java
+++ Chapter12/Cents.java
@@ -4,15 +4,22 @@ public class Cents{
     public static void main(String[] args){
 
     Scanner scan = new Scanner(System.in);
-    int cents, dollars;
+    int cents, dollars, quarters, dimes, nickels, pennies;
 
     System.out.println("Input cent value: ");
     cents = scan.nextInt();
 
     dollars = cents/100;
-    cents = cents%100;
+    quarters = (cents - 100*dollars)/25;
+    dimes = (cents - 100*dollars - 25*quarters)/10;
+    nickels = (cents - 100*dollars - 25*quarters - 10*dimes)/5;
+    pennies = cents - 100*dollars - 25*quarters - 10*dimes - 5*nickels;
 
-    System.out.println("That is " + dollars + " dollars and " + cents + " cents.");
+    System.out.println("Change due: " + cents + " cents, or " + 
+    dollars + " dollars, " + quarters + " quarters, " + 
+    dimes + " dimes, " + 
+    nickels + " nickels, and " + 
+    pennies + " pennies");
 
     }
 
