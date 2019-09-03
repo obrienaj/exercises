be8d76c4e0ba256b4205e1bb097f835420abb580
Allie O'Brien <obrienaj@s.dcsdk12.org>
2019-09-03 14:51:13 -0600

    Finish TimeUntil

--- TimeUntil.java
+++ TimeUntil.java
@@ -1,37 +1,53 @@
 import java.util.*;
-import java.time.LocalDataTime;
+import java.time.LocalDateTime;
 
 public class TimeUntil{
     public static void main(String[] args){
 
 Scanner scan = new Scanner(System.in);
 
-int futureHour, futureMinute, futureSecond;
+int presentHour, presentMinute, presentSecond;
 
-futureHour = LocalDataTime.now().getHour();
-futureMinute = LocalDataTime.now().getMinute();
-futureSecond = LocalDataTime.now().getSecond();     // do I have to import?
+presentHour = LocalDateTime.now().getHour();
+presentMinute = LocalDateTime.now().getMinute();
+presentSecond = LocalDateTime.now().getSecond();
+
+System.out.println("Current time is " + presentHour + ":" + presentMinute + ":" + presentSecond);
 
 System.out.println("Enter future time hh:mm:ss ");
 String future = new String(scan.nextLine());
 
 String hour, minute, second;
-hour = future.substring(1, 2);
-minute = future.substring(4, 5);
-second = future.substring(7, 8);    // convert string to int?
+hour = future.substring(0, 2);
+minute = future.substring(3, 5);
+second = future.substring(6, 8);
 
-intx = String.parseInt("future"); // do this some more
+int intHour = Integer.parseInt(hour);
+int intMinute = Integer.parseInt(minute);
+int intSecond = Integer.parseInt(second);
 
-hour = futureHour - hour;
-minute = futureMinute - minute;
-second = futureSecond - second;
+int hourDifference, minuteDifference, secondDifference;
+hourDifference = intHour - presentHour;
+minuteDifference = intMinute - presentMinute;
+secondDifference = intSecond - presentSecond;
 
-if (hour != 0)
-    System.out.println(hour + " hours");
-if (minute != 0)
-    System.out.println(minute + "minutes");
-if (second != 0)
-    System.out.println(second + "seconds");
+if (hourDifference < 0)
+    hourDifference = 24 + hourDifference;
+if (minuteDifference < 0) {
+    minuteDifference = 60 + minuteDifference;
+    hourDifference = hourDifference - 1;
+}
+if (secondDifference < 0) {
+    secondDifference = 60 + secondDifference;
+    minuteDifference = minuteDifference - 1;
+}
+
+if (hourDifference != 0)
+    System.out.println(hourDifference + " hours");
+if (minuteDifference != 0)
+    System.out.println(minuteDifference + " minutes");
+if (secondDifference != 0)
+    System.out.println(secondDifference + " seconds");
 
 
 
