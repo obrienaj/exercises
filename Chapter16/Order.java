01f63a1dddaea126e03678ea7b36aa26e6d3d0ea
Allie O'Brien <obrienaj@s.dcsdk12.org>
2019-09-06 14:24:30 -0600

    Finish order

--- Chapter16/Order.java
+++ Chapter16/Order.java
@@ -1,8 +1,27 @@
 import java.util.Scanner;
 
 public class Order{
-    public static void main(String[] args);
+    public static void main(String[] args){
 
+Scanner scan = new Scanner(System.in);
 
+System.out.println("Enter quantity of bolts:");
+int bolts = scan.nextInt();
 
+System.out.println("Enter quantity of nuts:");
+int nuts = scan.nextInt();
+
+System.out.println("Enter quantity of washers:");
+int washers = scan.nextInt();
+
+if (nuts >= bolts)
+    System.out.println("Check order: too few nuts");
+    
+if (washers >= 2*bolts)
+    System.out.println("Check order: too few washers");
+
+int price = 5*bolts + 3*nuts + washers;
+System.out.println("Price: " + price);
+
+}
 }
