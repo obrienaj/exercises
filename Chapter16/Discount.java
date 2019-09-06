b1d6f173940ef3ddb6cd25064740915c5bda8ab7
Allie O'Brien <obrienaj@s.dcsdk12.org>
2019-09-06 14:06:16 -0600

    Finish Discount

--- Chapter16/Discount.java
+++ Chapter16/Discount.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
+
 public class Discount{
     public static void main(String[] args){
 
+Scanner scan = new Scanner(System.in);
+
+System.out.println("Enter cost of purchases in cents:");
+int cost = scan.nextInt();
+
+int finalCost;
+
+if (cost > 1000)
+    finalCost =  (cost)/10*9;
+else 
+    finalCost = cost;
+
+System.out.println("Discounted price: " + finalCost);
 
 
-        
     }
 
 
