7678ff646a9d7d00b615a2df63fb8f9dac558760
Allie O'Brien <obrienaj@s.dcsdk12.org>
2019-09-09 14:40:38 -0600

    Finish Delicatessen

--- Chapter17/Delicatessen.java
+++ Chapter17/Delicatessen.java
@@ -1,8 +1,35 @@
 import java.util.Scanner;
 public class Delicatessen{
     public static void main(String[] args){
-    
 
+Scanner scan = new Scanner(System.in);
+
+System.out.println("Enter item name:");
+String item = scan.nextLine();
+
+System.out.println("Enter item price in cents:");
+int price = scan.nextInt();
+
+int delivery = 42;
+
+if (price < 1000)
+    delivery = 200;
+
+if (price >= 1000)
+    delivery = 0;
+
+System.out.println("Express delivery? (0=no, 1=yes)");
+int express = scan.nextInt();
+
+if (express == 1)
+    delivery = delivery + 300;
+
+int total = price + delivery;
+
+System.out.println("Invoice:");
+System.out.println("    " + item + "    " + price);
+System.out.println("    Delivery    " + delivery);
+System.out.println("    Total   " + total);
 
 
 
