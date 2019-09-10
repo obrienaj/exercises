e192059159fbc0106a11a402dea2e79ae5718870
Allie O'Brien <obrienaj@s.dcsdk12.org>
2019-09-10 14:34:21 -0600

    Finish Chapter 17

--- Chapter17/Dump.java
+++ Chapter17/Dump.java
@@ -3,10 +3,22 @@ import java.util.Scanner;
 public class Dump{
     public static void main(String[] args){
 
+Scanner scan = new Scanner(System.in);
+
+System.out.println("Enter trash weight:");
+double weight = scan.nextInt();
+
+double cost;
+
+if (weight <= 200)
+    cost = weight*.1; 
+else
+    cost = 20 + .08*(weight - 200);
+
+System.out.println("Cost: " + cost);
 
 
 
-        
     }
 
 
