210fa7b7bdc4828e21b1456696971e4e49ad4652
Allie O'Brien <obrienaj@s.dcsdk12.org>
2019-09-10 14:09:28 -0600

    Finish SteamEngine

--- Chapter17/SteamEngine.java
+++ Chapter17/SteamEngine.java
@@ -3,10 +3,26 @@ import java.util.Scanner;
 public class SteamEngine{
     public static void main(String[] args){
 
+Scanner scan = new Scanner(System.in);
+
+System.out.println("Enter air temperature in degrees K:");
+double tair = scan.nextInt();
+
+System.out.println("Enter steam temperature in degrees K:");
+double tsteam = scan.nextInt();
+
+double max;
+
+if (tsteam < 373)
+    max = 0;
+else
+    max = 1 - tair/tsteam;
+
+System.out.println("Maximum steam engine efficiency: " + max);
 
 
 
-        
+
     }
 
 
