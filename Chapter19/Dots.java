9f9f05da5059f29c31b1247c307a8f984b9f3646
Allie O'Brien <obrienaj@s.dcsdk12.org>
2019-09-11 14:39:57 -0600

    Finish Dots

--- Chapter19/Dots.java
+++ Chapter19/Dots.java
@@ -3,10 +3,25 @@ import java.util.Scanner;
 public class Dots{
     public static void main(String[] args){
 
+Scanner scan = new Scanner(System.in);
 
+System.out.println("Enter first word:");
+String first = scan.nextLine();
 
+System.out.println("Enter second word:");
+String second = scan.nextLine();
 
-        
+int length = first.length() + second.length();
+
+String dots = ".";
+
+while (length < 29) {
+    dots = dots + ".";
+    length = length + 1;
+}
+
+System.out.println(first + dots + second);
+
     }
 
 }
