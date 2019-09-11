9c2429225edd99c8daeaf6d782c129e95edbb7a3
Allie O'Brien <obrienaj@s.dcsdk12.org>
2019-09-11 14:22:53 -0600

    Finish Sum

--- Chapter19/Sum.java
+++ Chapter19/Sum.java
@@ -3,10 +3,25 @@ import java.util.Scanner;
 public class Sum{
     public static void main(String[] args){
 
+Scanner scan = new Scanner(System.in);
+
+System.out.println("Enter N:");
+int n = scan.nextInt();
+
+int sum = 0;
+int count = 1;
+
+while (count <= n) {
+    sum = sum + count;
+    count = count +1;
+}
+
+System.out.println("Loop sum = " + sum);
+
+sum = (n*(n+1)) / 2;
+System.out.println("Formula sum = " + sum);
 
 
-
-        
     }
 
 }
