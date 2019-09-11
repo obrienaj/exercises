5e8c7678073d1306ccd67276a8fbdbac69123279
Allie O'Brien <obrienaj@s.dcsdk12.org>
2019-09-11 14:12:11 -0600

    Finish Integers

--- Chapter19/Integers.java
+++ Chapter19/Integers.java
@@ -3,10 +3,22 @@ import java.util.Scanner;
 public class Integers{
     public static void main(String[] args){
 
+Scanner scan = new Scanner(System.in);
+
+System.out.println("Enter starting value:");
+int start = scan.nextInt();
+
+System.out.println("Enter ending value:");
+int end = scan.nextInt();
+
+int count = start;
+
+while (count <= end) {
+    System.out.println(count);
+    count = count + 1;
+}
 
 
-
-        
     }
 
 }
