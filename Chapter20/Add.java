d00aed7d6b4f9a3c4270bfb7205e8fb988adf716
Allie O'Brien <obrienaj@s.dcsdk12.org>
2019-09-13 14:21:45 -0600

    Finish Add

--- Chapter20/Add.java
+++ Chapter20/Add.java
@@ -3,10 +3,24 @@ import java.util.Scanner;
 public class Add{
     public static void main(String[] args){
 
+Scanner scan = new Scanner(System.in);
 
+System.out.println("How many integers will be added?");
+int terms = scan.nextInt();
 
+int count = 1;
+int integer = 0;
+int sum = 0;
 
-        
+while (count <= terms) {
+    System.out.println("Enter an integer:");
+    integer = scan.nextInt();
+    sum = sum + integer;
+    count++ ;
+}
+
+System.out.println("The sum is " + sum);
+
     }
 
 
