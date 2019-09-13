4dd1c4383ce610dc58c393e35a9fb4da44cb8364
Allie O'Brien <obrienaj@s.dcsdk12.org>
2019-09-13 14:30:13 -0600

    Finish Harmonic

--- Chapter20/Harmonic.java
+++ Chapter20/Harmonic.java
@@ -3,10 +3,21 @@ import java.util.Scanner;
 public class Harmonic{
     public static void main(String[] args){
 
+Scanner scan = new Scanner(System.in);
 
+System.out.println("Enter N:");
+int n = scan.nextInt();
 
+int count = 1;
+double sum = 0;
 
-        
+while (count <= n){
+    sum = sum + 1.0/count;
+    count++;
+}
+
+System.out.println("The sum is " + sum);
+
     }
 
 
