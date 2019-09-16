bfb603fc77cdd7c244a14f1268b47f4d8570b9b9
Allie O'Brien <obrienaj@s.dcsdk12.org>
2019-09-16 14:31:17 -0600

    Finish Power

--- Chapter21/Power.java
+++ Chapter21/Power.java
@@ -1,9 +1,32 @@
 import java.util.Scanner;
 
 public class Power{
-    public static void main (String[] args){
+    public static void main(String[] args){
+
+Scanner scan = new Scanner(System.in);
+
+System.out.println("Enter X:");
+double x = scan.nextDouble();
+
+System.out.println("Enter n:");
+int n = scan.nextInt();
+
+if (n < 0)
+    System.out.println("N must be a positive integer");
+else {
+
+double power = 1;
+int count = 1;
+
+while (count <= n){
+    power = power * x;
+    count++;
+}
+
+System.out.println(x + " raised to the power of " + n + " is " + power);
 
 
+}
 
 
     }
