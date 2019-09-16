aba61196f599c9e77b0929370e7a35904faef184
Allie O'Brien <obrienaj@s.dcsdk12.org>
2019-09-16 14:47:48 -0600

    Finish Chapter 21

--- Chapter21/MultQuiz.java
+++ Chapter21/MultQuiz.java
@@ -1,12 +1,38 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class MultQuiz{
     public static void main (String[] args){
 
+Scanner scan = new Scanner(System.in);
+
+Random random = new Random();
+
+int count = 1;
+int x, y, product, response;
+int correct = 0;
+
+while (count <= 10){
+    x = random.nextInt(10);
+    y = random.nextInt(10);
+    product = x*y;
+    System.out.println("What is " + x + " * " + y + "?");
+    response = scan.nextInt();
+    
+    if (response == product){
+        System.out.println("Right!");
+        correct = correct + 1;
+        }
+    else
+        System.out.println("Wrong. " + x + " * " + y + " is " + product);
+
+    count++;
+}
+
+System.out.println("You got " + correct + " out of 10 questions correct!");
 
 
 
-        
     }
 
 
