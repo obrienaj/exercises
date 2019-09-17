663ac44694f34b4f3c4e7c57571c255df99a3892
Allie O'Brien <obrienaj@s.dcsdk12.org>
2019-09-17 14:23:02 -0600

    Finish Chapter 22

--- Chapter22/Mpg.java
+++ Chapter22/Mpg.java
@@ -3,10 +3,39 @@ import java.util.Scanner;
 public class Mpg{
     public static void main (String[] args){
 
+Scanner scan = new Scanner(System.in);
+
+int milesI = 42;
+int milesF, gallons;
+float mpg;
+
+while (milesI >= 0){
+
+    System.out.println("Initial miles:");
+    milesI = scan.nextInt();
+
+    if (milesI < 0)
+        System.out.println("Goodbye!");
+
+    else {
+    
+    System.out.println("Final miles:");
+    milesF = scan.nextInt();
+
+    System.out.println("Gallons used:");
+    gallons = scan.nextInt();
+
+    mpg = (milesF - milesI) / gallons;
+    System.out.println("Miles per gallon: " + mpg);
+
+    System.out.println(" ");
+}
+}
 
 
 
-        
+
+
     }
 
 
