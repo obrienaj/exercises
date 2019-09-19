1f777965611e0056e0c38e6b323364dfa2f8fe99
Allie O'Brien <obrienaj@s.dcsdk12.org>
2019-09-19 07:48:32 -0600

    Finish Chapter 23

--- Chapter23/Euler.java
+++ Chapter23/Euler.java
@@ -5,6 +5,23 @@ public class Euler{
 
 Scanner scan = new Scanner(System.in);
 
+System.out.println("Enter x:");
+int x = scan.nextInt();
+
+int count = 1;
+double denominator = 42;
+double numerator = 1;
+double term = 42;
+double sum = 1;
+
+while (Math.abs(term) < 1.0E-12){
+    denominator = count*count;
+    numerator = numerator*x;
+    term = (numerator)/(count*denominator);
+    sum = sum + term;
+    count++;
+}
+System.out.println("Calculated e^x: " + sum);
 
 
     }
