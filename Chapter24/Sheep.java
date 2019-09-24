9b0bc462a1e52fc1a6a496d5ad863ac7424289a4
Allie O'Brien <obrienaj@s.dcsdk12.org>
2019-09-24 14:33:09 -0600

    Finish Sheep

--- Chapter24/Sheep.java
+++ Chapter24/Sheep.java
@@ -1,10 +1,19 @@
 public class Sheep{
     public static void main(String[] args){
 
+double n = 0;
+int t;
+double power = 1;
+
+for (t = 0; n < 80; t++){
+    n = 220 / (1 + 10*power);
+    power = power * .83;
+    System.out.println("After " + t + " years, the population of the herd will be " + n);
+}
+System.out.println("The herd will need to be supervised for " + (t-1) + " years");
 
 
 
-        
     }
 
 
