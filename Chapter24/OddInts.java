d229bf1cf9758c8da70e1d099d3167a16605318a
Allie O'Brien <obrienaj@s.dcsdk12.org>
2019-09-24 14:16:11 -0600

    Finish OddInts

--- Chapter24/OddInts.java
+++ Chapter24/OddInts.java
@@ -5,6 +5,20 @@ public class OddInts{
 
 Scanner scan = new Scanner(System.in);
 
+System.out.println("Enter integer:");
+int n = scan.nextInt();
+
+int sum = 0;
+int count;
+
+for (count = 1; count <= n; count += 2)
+    sum = sum + count;
+
+int square = n*n;
+
+System.out.println("Sum of all odd integers from 1 to N: " + sum);
+System.out.println("Square: " + square);
+
 
 
 
