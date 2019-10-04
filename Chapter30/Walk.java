831562b0cae06f28bc76479f92871af7ae3532ee
Allie O'Brien <obrienaj@s.dcsdk12.org>
2019-10-04 14:20:22 -0600

    Finish 2d

--- Chapter30/Walk.java
+++ Chapter30/Walk.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class 2d{
+public class Walk{
     public static void main(String[] args){
 
 Scanner scan = new Scanner(System.in);
@@ -13,8 +13,8 @@ System.out.println("Enter number of iterations:");
 int iteration = scan.nextInt();
 
 for (int i = 1; i <= iteration; i++){
-    x = x + rand.nextDouble(2) - 1;
-    y = y + rand.nextDouble(2) - 1;
+    x = x + 2*rand.nextDouble() - 1;
+    y = y + rand.nextDouble() - 1;
 }
 
 System.out.println("Position: (" + x + ", " + y + ")");
