791682a6509a94887b7f9447cd915635ebd9ddba
Allie O'Brien <obrienaj@s.dcsdk12.org>
2019-10-09 13:29:34 -0600

    Start Farkle

---
+++ Farkle.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Farkle{
+    public static void main(String[] args){
+
+Scanner scan = new Scanner(System.in);
+Random rand = new Random();
+
+
+
+
+
+
+
+    }
+
+
+}
