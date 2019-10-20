3e53fb17eb420020aedd77e02642848873f0e4d4
Allie O'Brien <obrienaj@s.dcsdk12.org>
2019-10-19 21:57:10 -0600

    Finish Chapter 25

--- Chapter25/SqTri.java
+++ Chapter25/SqTri.java
@@ -3,9 +3,28 @@ import java.util.Scanner;
 public class SqTri{
     public static void main(String[] args){
 
+Scanner scan = new Scanner(System.in);
+
+System.out.println("Enter upper limit:");
+long limit = scan.nextInt();
+
+int sum = 0;
+double i = 42;
+
+System.out.println("Square triangular numbers: ");
+
+for (int n = 1; sum <= limit; n++){
+    sum = sum + n;
+    i = Math.sqrt(sum);
+
+    while (i > 0.00000042)
+        i = i - 1;
+    
+    if (i <= 0.00000042 && i > -0.00000042)
+        System.out.print(sum + ", ");
+}
 
 
 
-        
     }
 }
