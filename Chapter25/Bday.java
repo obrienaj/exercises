b1d1f31dc4d9e7e4b0e346c235fa49408d7f7e0b
Allie O'Brien <obrienaj@s.dcsdk12.org>
2019-10-19 19:32:50 -0600

    Finish Bday

--- Chapter25/Bday.java
+++ Chapter25/Bday.java
@@ -3,9 +3,26 @@ import java.util.Scanner;
 public class Bday{
     public static void main(String[] args){
 
+Scanner scan = new Scanner(System.in);
 
+System.out.println("How many guests are present?");
+int guests = scan.nextInt();
 
+double chance = 1;
 
-        
+for (int n = 1; n <= guests; n++)
+    chance = chance * ((365.0 - (n-1)) / 365.0);
+
+System.out.println("Probability of a shared birthday: " + (1.0 - chance)*100 + "%");
+
+double chance2 = 1;
+int answer = 42;
+
+for (int n = 1; chance2 >= .5; n++){
+    chance2 = chance2 * ((365.0 - (n-1)) / 365.0);
+    answer = n;
+}
+System.out.println("In a party with " + answer + " guests, there is a " + (1.0 - chance2)*100 + "% chance of at least one shared birthday.");
+
     }
 }
