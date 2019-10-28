37598baa26c0a617947a92cdacc6ff88c8bf1d62
Allie O'Brien <obrienaj@s.dcsdk12.org>
2019-10-28 14:49:56 -0600

    Edit Cone

--- Chapter47/Cone.java
+++ Chapter47/Cone.java
@@ -1,9 +1,34 @@
 public class Cone{
     public static void main(String[] args){
-
-
-
-
         
+        Cone cone = new Cone(4, 12);
+        
+        System.out.print("Slant height: " + cone.slantHeight());
+        System.out.println("    Angle: " + cone.angle());
+
     }
+
+
+    public Cone(double r, double h){
+        this.r = r;
+        this.h = h;
+    }
+
+
+    public double slantHeight(){
+
+        return Math.sqrt(r*r + h*h);
+
+    }
+
+
+
+    public double angle(){
+
+        return Math.arctan(r/h);
+
+    }
+
+
+    
 }
