2cc45e1d1a9027518460dc618e687e141a4b0945
Allie O'Brien <obrienaj@s.dcsdk12.org>
2019-11-20 14:39:01 -0700

    Finish Array2

---
+++ Chapter60/Array2.java
@@ -0,0 +1,15 @@
+public class Array2{
+
+    public static void main(String[] args){
+
+        int[] val = {13, -4, 82, 17}; 
+        int[] twice = {2 * val[0], 2 * val[1], 2 * val[2], 2 * val[3]};
+        
+        System.out.println( "Original Array: " 
+            + val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
+        
+
+        System.out.println( "New Array: " 
+            + twice[0] + " " + twice[1] + " " + twice[2] + " " + twice[3] );
+    }
+}
