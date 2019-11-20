29b6bf5085ff47d4f1c37bb5727345fe4d05e6e9
Allie O'Brien <obrienaj@s.dcsdk12.org>
2019-11-20 14:04:44 -0700

    Finish Chapter 54

--- Chapter54/Combos.java
+++ Chapter54/Combos.java
@@ -11,5 +11,35 @@ public class Combos{
 
         System.out.println("Enter R:");
         int r = scan.nextInt();
+
+        long ncr = ncr(n, r);
+
+        if (ncr > 0)
+            System.out.println("NCR = " + ncr);
+    }
+
+    public static long ncr(int n, int r){
+
+        if (n >= 0 && n < 21 && r >= 0 && r < 21){ 
+            return factorial(n) / (factorial(r) * factorial(n-r));
+        } else if (n < 0 || r < 0){
+            System.out.println("Error: negative input");
+            return -1;
+        } else{
+            System.out.println("Error: out of bounds");
+            return -1;
+        }        
+    }
+
+    public static long factorial(int i){
+        
+        if (i == 0) {
+            return 1;
+        } else {
+            long fct = 1;
+            for ( int j = 1; j <= i; j++ )
+            fct *= j;
+            return fct;
+        }
     }
 }
