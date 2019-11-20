91cc68c942efe569bfdcd49f632607ffc21300ee
Allie O'Brien <obrienaj@s.dcsdk12.org>
2019-11-20 13:24:36 -0700

    Start Combos

---
+++ Chapter54/Combos.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Combos{
+
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter N:");
+        int n = scan.nextInt();
+
+        System.out.println("Enter R:");
+        int r = scan.nextInt();
+    }
+}
