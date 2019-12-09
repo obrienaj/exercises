64a556f1a999d5ac3916d0076d724073658a413e
Allie O'Brien <obrienaj@s.dcsdk12.org>
2019-12-09 14:33:26 -0700

    Finish Coffee

--- Chapter61/Coffee.java
+++ Chapter61/Coffee.java
@@ -1,9 +1,41 @@
 import java.io.*;
+import java.util.Scanner;
 
 public class Coffee{
 
     public static void main(String[] args){
 
-        int[] ph = new int[13];
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("How many coffees tested?");
+        final int x = scan.nextInt();
+
+        double[] ph = new double[x];
+        double sum = 0;
+
+        for (int j = 0; j < x; j++){
+            
+            System.out.print("pH[" + j + "] = ");
+            ph[j] = scan.nextDouble();
+            sum += ph[j];    
+        }
+
+        double avg1 = sum / x;
+        double trash = ph[0];
+
+        System.out.println("Average pH: " + avg1);
+
+        for (int j = 1; j < x; j++){
+
+            if (Math.abs(avg1 - ph[j]) > Math.abs(avg1 - trash))
+                trash = ph[j];
+        }
+
+        System.out.println("Most distant value: " + trash);
+
+        double avg2 = (sum - trash) / (x - 1);
+
+        System.out.println("New average pH: " + avg2);
+        
     }
 }
