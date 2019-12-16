6beeea3546de688023a3972290995aa6f49de69b
Allie O'Brien <obrienaj@s.dcsdk12.org>
2019-12-16 14:29:30 -0700

    Create WeatherInfo

---
+++ PencilPractice/WeatherInfo.java
@@ -0,0 +1,13 @@
+public class WeatherInfo
+{
+    // Class variables
+    GeoLocation geo;
+    int temp;
+
+    // Constructor
+    public WeatherInfo(GeoLocation geo, int temp)
+    {
+        this.geo = geo;
+        this.temp = temp;
+    }
+}
---
+++ PencilPractice/WeatherInfoTester.java
@@ -0,0 +1,7 @@
+public class WeatherInfoTester
+{
+    public static void main(String[] args)
+    {
+        WeatherInfo climate1 = new WeatherInfo(new GeoLocation(latitude, longitude), temp)        
+    }
+}
